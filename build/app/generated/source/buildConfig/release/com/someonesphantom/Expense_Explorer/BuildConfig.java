edd5e26d53314be47d901f98d400852f5aad48dc
someonesphantom <venkatin2sl@gmail.com>
2025-05-21 15:07:09 +0530

    adding files

---
+++ build/app/generated/source/buildConfig/release/com/someonesphantom/Expense_Explorer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.someonesphantom.Expense_Explorer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.someonesphantom.Expense_Explorer";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
